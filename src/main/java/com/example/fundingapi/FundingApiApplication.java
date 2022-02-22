cb5924c708323be98ff67e17c24012dac722664b
kimmmm <khjjung1124@gmail.com>
2022-02-22 13:42:13 +0900

    펀딩 이용자를 위한 펀딩 서비스

---
+++ src/main/java/com/example/fundingapi/FundingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fundingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fundingapi/FundingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fundingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
