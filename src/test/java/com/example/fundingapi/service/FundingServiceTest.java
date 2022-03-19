77637b4c1eb352eef06f9dd7afda3d20380d4dc4
hyunjungkimm <khjjung1124@gmail.com>
2022-03-19 15:58:52 +0900

    단위테스트 FundingServiceTest - sold out 테스트 완료

--- src/test/java/com/example/fundingapi/service/FundingServiceTest.java
+++ src/test/java/com/example/fundingapi/service/FundingServiceTest.java
@@ -2,6 +2,7 @@ package com.example.fundingapi.service;
 
 import com.example.fundingapi.data.FundingRequest;
 import com.example.fundingapi.domain.Product;
+import com.example.fundingapi.domain.User;
 import com.example.fundingapi.error.ErrorCode;
 import com.example.fundingapi.exception.service.funding.FundingServiceException;
 import com.example.fundingapi.repository.ProductRepository;
@@ -12,21 +13,22 @@ import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FundingServiceTest {
 
     @InjectMocks
-    private FundingService fundingService;
+    private FundingServiceImpl fundingService;
 
     @Mock
     private ProductRepository productRepository;
@@ -34,8 +36,28 @@ public class FundingServiceTest {
     @BeforeAll
     void setUp(){
         MockitoAnnotations.openMocks(this);//이 클래스  - this
+
+
     }
 
+    @Test
+    @DisplayName("sold out 테스트")
+    public void test1() {
+
+        when(
+            productRepository.findById(1002L)
+        ).thenReturn(
+            Optional.of(new Product())
+        );
+
+        Long userId = 1L;
+        Long productId = 1002L;
+        FundingRequest fundingRequest = new FundingRequest(1000);
+
+        FundingServiceException exception = assertThrows(FundingServiceException.class, ()-> fundingService.productFunding(userId, productId, fundingRequest));
+
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.SOLD_OUT);
+    }
 
     @Test
     @DisplayName("펀딩하기 테스트")
